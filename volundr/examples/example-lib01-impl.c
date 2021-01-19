29e700409ddd0ad489f32317d57d951624f40f4e
Carlos Carvalho <carloslack@gmail.com>
2021-01-19 14:35:18 +0000

    trojan: Include example for ET_DYN
    ET_DYN trojan soon

---
+++ volundr/examples/example-lib01-impl.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void showme(void)
+{
+    printf("Hoooray here we are: %s:%d\n", __FILE__, __LINE__);
+}
+
---
+++ volundr/examples/example-lib01.c
@@ -0,0 +1,6 @@
+extern void showme(void);
+int main(int argc, char** argv)
+{
+    showme();
+    return 0;
+}
