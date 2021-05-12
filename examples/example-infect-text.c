a76bcfe3f36d014e48ed507d236f002b35174c48
Carlos Carvalho <carloslack@gmail.com>
2021-05-12 17:31:02 +0100

    Fix crash in example if inf_load fail

--- examples/example-infect-text.c
+++ examples/example-infect-text.c
@@ -83,18 +83,20 @@ static bool doit(const char *binfile, const char *trojan) {
         long magic = (long)0x1122334455667788;
 
         infect_t *inf = inf_load(elfo, m1, magic, &src_map);
-        if (inf_scan_segment(inf)) {
-            if ((rc = inf_load_and_patch(inf)) == true) {
-                // Make sure it is written
-                if (file_sync_target(&map))
-                    printf("Done!\nTry running %s\n", file);
+        if (inf) {
+            if (inf_scan_segment(inf)) {
+                if ((rc = inf_load_and_patch(inf)) == true) {
+                    // Make sure it is written
+                    if (file_sync_target(&map))
+                        printf("Done!\nTry running %s\n", file);
+                }
+                else
+                    printf("Failed :(\n");
             }
-            else
-                printf("Failed :(\n");
+            free(inf->trojan);
+            free(inf);
         }
 
-        free(inf->trojan);
-        free(inf);
         assert(elf_destroy_all(elfo));
     }
 
